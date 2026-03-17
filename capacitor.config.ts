d89f5217313bf83a2008c06b844294567a0045d1
alexispata1998-spec <alexispata1998@gmail.com>
2026-03-16 22:42:29 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'mi.primer.ios.proyec',
+  appName: 'mi-primer-ios-proyec',
+  webDir: 'dist'
+};
+
+export default config;
