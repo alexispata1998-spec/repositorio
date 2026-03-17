0da2f6915263e9d3f47d6c9870b6ddafb5100e0d
alexispata1998-spec <alexispata1998@gmail.com>
2026-03-17 00:17:49 -0500

    camara

--- src/views/Tab1Page.vue
+++ src/views/Tab1Page.vue
@@ -1,23 +1,41 @@
 <template>
   <ion-page>
     <ion-header>
-      <ion-toolbar>
-        <ion-title>Tab 1</ion-title>
+      <ion-toolbar color="primary">
+        <ion-title>Inicio de Sesión</ion-title>
       </ion-toolbar>
     </ion-header>
-    <ion-content :fullscreen="true">
-      <ion-header collapse="condense">
-        <ion-toolbar>
-          <ion-title size="large">Tab 1</ion-title>
-        </ion-toolbar>
-      </ion-header>
 
-      <ExploreContainer name="Tab 1 page" />
+    <ion-content class="ion-padding">
+      <div style="text-align: center;">
+        <h2>Bienvenido</h2>
+        <p>Ingresa tus datos para continuar</p>
+      </div>
+
+      <ion-item>
+        <ion-label position="floating">Correo Electrónico</ion-label>
+        <ion-input type="email" placeholder="correo@ejemplo.com"></ion-input>
+      </ion-item>
+
+      <ion-item class="ion-margin-top">
+        <ion-label position="floating">Contraseña</ion-label>
+        <ion-input type="password" placeholder="********"></ion-input>
+      </ion-item>
+
+      <ion-button expand="full" shape="round" class="ion-margin-top">
+        Ingresar
+      </ion-button>
+
+      <ion-button expand="full" shape="round" color="light">
+        ¿No tienes cuenta? Regístrate
+      </ion-button>
     </ion-content>
   </ion-page>
 </template>
 
 <script setup lang="ts">
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
-import ExploreContainer from '@/components/ExploreContainer.vue';
+import { 
+  IonPage, IonHeader, IonToolbar, IonTitle, 
+  IonContent, IonItem, IonLabel, IonInput, IonButton 
+} from '@ionic/vue';
 </script>
